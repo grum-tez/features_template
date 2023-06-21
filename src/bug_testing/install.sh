0f74ab2b19a94ee84f6d96e01d7d72f8a3c84876
grum-tez <grum.tez@gmail.com>
2023-06-21 16:51:03 +1200

    add chmod to bug_testing

--- src/bug_testing/install.sh
+++ src/bug_testing/install.sh
@@ -21,4 +21,5 @@ sudo apt-get update -y \
 && sudo apt-get install software-properties-common -y \
 && sudo add-apt-repository -y "ppa:serokell/tezos" \
 && sudo apt-get update \
+&& sudo chmod 777 /sys/devices/breakpoint/uevent \
 && sudo apt-get install -y tezos-client
