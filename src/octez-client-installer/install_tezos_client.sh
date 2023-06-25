0c1eb09d2b63e0ad10dba246b389631f14d51e2c
grum-tez <grum.tez@gmail.com>
2023-06-25 18:45:22 +1200

    Run an install script off onCreateCommand

---
+++ src/octez-client-installer/install_tezos_client.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo apt-get update -y
+sudo apt-get install software-properties-common -y
+sudo add-apt-repository -y "ppa:serokell/tezos"
+sudo apt-get update -y
+sudo apt-get install -y tezos-client
